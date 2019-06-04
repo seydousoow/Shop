9ad6956fc2f13058b617a45e77b7dcfddf3894b4
seydousoow <seydousoow@gmail.com>
2019-06-04 01:10:33 +0000

    comm

--- src/main/java/com/sid/repositories/ShoeRepository.java
+++ src/main/java/com/sid/repositories/ShoeRepository.java
@@ -1,17 +1,14 @@
 package com.sid.repositories;
 
-import java.util.List;
-
+import com.sid.entities.Shoe;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.sid.entities.Shoe;
+import java.util.List;
 
 @Repository
 public interface ShoeRepository extends MongoRepository<Shoe, String> {
-
 	List<Shoe> findByBrandEquals(@Param("brand") String brand);
 	List<Shoe> findByModelEquals(@Param("model") String model);
 	Boolean findByCodeExists(@Param("code") String code);
